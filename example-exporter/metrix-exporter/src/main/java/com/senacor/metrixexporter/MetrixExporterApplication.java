10949c39df85b9ce7880d5ec111d9d0532f8a461
pschulz <patrick@schulz-de.com>
2022-10-12 18:23:33 +0200

    example app added

---
+++ example-exporter/metrix-exporter/src/main/java/com/senacor/metrixexporter/MetrixExporterApplication.java
@@ -0,0 +1,13 @@
+package com.senacor.metrixexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetrixExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetrixExporterApplication.class, args);
+	}
+
+}
---
+++ example-exporter/metrix-exporter/src/test/java/com/senacor/metrixexporter/MetrixExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacor.metrixexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetrixExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
